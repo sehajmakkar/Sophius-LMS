db0063cedaf336ec09592e2e918957a19c020803
sehajmakkar <codewithsehaj@gmail.com>
2025-02-25 18:12:37 +0530

    userSchema

---
+++ server/models/user.model.js
@@ -0,0 +1,118 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "Name is required"],
+      trim: true,
+      match: [
+        /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+        "Please provide a valid email.",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleast 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["student", "instructor", "admin"],
+        message: "Please Select a valid role",
+      },
+      default: "student",
+    },
+    avatar: {
+      type: String,
+      default: "default-avatar.png",
+    },
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannot exceed 200 characters"],
+    },
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+        enrolledAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    createdCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    lastActive: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// hashing the password
+// pre method (hooks)
+// dont use arrow functions as they need context
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// compare password
+// why not next() in this too?
+userScheme.methods.comparePassword = async function (enterPassword) {
+  await bcrypt.compare(enterPassword, this.password);
+};
+
+// reset password
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10min
+  return resetToken;
+};
+
+// last active
+userSchema.methods.updateLastActive = function () {
+  this.lastActive = Date.now();
+  return this.lastActive({ validateBeforeSave: false });
+};
+
+// virtual fields for total enrolled courses
+userSchema.virtual("totalEnrolledCourses").get(function () {
+  return this.enrolledCourses.length;
+});
+
+export const User = mongoose.model("User", userSchema);
